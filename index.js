6a4dac7b23673c19b1bd0f63d6f70ff02e5872a3
thanawat <tha_chookes@hotmail.com>
2016-02-06 01:41:38 +0700

    only mic

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ var request = app.textRequest('hello');
 request.on('response', function(response) {
     console.log(response);
 });
-
 request.on('error', function(error) {
     console.log(error);
 });
+
 
 request.end()
 
---
+++ public/ng.js
@@ -0,0 +1,5 @@
+angular.module('app', [])
+  .controller('TodoListController', function() {
+    var app = this;
+    app.test = "koy"
+  });
